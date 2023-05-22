374508b2debc03915dc11e1ca45ab88a104cb084
chainmaster05 <hongyuxiao05@gmail.com>
2023-05-22 16:25:32 -0700

    fea: Update about part

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is ABU SAID. I'm a web designer and developer based in Dhaka, Bangladesh.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
+    description1: "My name is Hong Yu Xiao. I'm a AI and Machine Learning Engineer based in Hong Kong.",
+    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. I love Artificial Intellingence and all the algorithm and technologies relate to AI. I am also very good at Mathematics",
 }
